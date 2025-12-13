fffe37f630ce6a3e8855273866675c8e600d83a5
DennisonCc <dachalacan1@espe.edu.ec>
2025-12-13 16:14:26 -0500

    add base class for PedidoController & PedidoServiceImpl

---
+++ PedidoService/src/main/java/ec/edu/espe/PedidoService/services/PedidoService.java
@@ -0,0 +1,4 @@
+package ec.edu.espe.PedidoService.services;
+
+public interface PedidoService {
+}
---
+++ PedidoService/src/main/java/ec/edu/espe/PedidoService/services/impl/PedidoServiceImpl.java
@@ -0,0 +1,4 @@
+package ec.edu.espe.PedidoService.services.impl;
+
+public class PedidoServiceImpl {
+}
